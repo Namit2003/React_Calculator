cc6b4c91016f61dda50f4cb80434d533f1a3430b
Namit <namit.k.patel@gmail.com>
2023-11-20 13:11:24 +0530

    11 passed

--- React_Calculator/src/App.jsx
+++ React_Calculator/src/App.jsx
@@ -6,31 +6,108 @@ import './App.css'
 function App() {
 
   const [question, setQuestion] = useState("")
-  const [answer, setAnswer] = useState(0)
+  const [answer, setAnswer] = useState('0')
 
   const handleClick = (val) => {
-    setQuestion(question + val)
+    // console.log(val)
+    if (val === '0' && question === '0') {
+      return;
+    }
+    if (val !== '0' && question === '0') {
+      setQuestion(val)
+      setAnswer(val)
+      return;
+    }
+    if ((val === '/' || val === '*') && question === '') {
+      return;
+    }
+    const symbols = ['+', '-', '/', '*']
+    if (val === '-') {
+      setQuestion(question + val)
+    }
+    else if (question.length > 2 && symbols.includes(val) && symbols.includes(question[question.length - 1]) && symbols.includes(question[question.length - 2])) {
+      let str1 = question.slice(0, -2) + val;
+      setQuestion(str1)
+    }
+    else if (symbols.includes(val) && symbols.includes(question[question.length - 1])) {
+      let str1 = question.slice(0, -1) + val;
+      setQuestion(str1)
+    }
+    else {
+      setQuestion(question + val)
+    }
+    // for symbols
+    if (symbols.includes(val)) {
+      setAnswer(val)
+    }
+    // for first entered value
+    else if (question === '' || answer === 0) {
+      setAnswer(val)
+    }
+    // extracting string after last symbol
+    else {
+      let str = ''
+      let l = question.length - 1;
+      while (l >= 0 && !symbols.includes(question[l])) {
+        str += question[l]
+        l--;
+      }
+      const reversedString = str.split('').reverse().join('');
+      setAnswer(reversedString + val)
+    }
   }
 
   const calculateAnswer = () => {
-    setAnswer(eval(question))
-    setQuestion(question+"="+eval(question))
+    if (question === '') {
+      return;
+    }
+    setAnswer(Number(eval(question)))
+    setQuestion(question + "=" + eval(question))
   }
 
   const clearDisplay = () => {
     setQuestion("")
-    setAnswer(0)
+    setAnswer('0')
+  }
+
+  const handleDot = () => {
+    const symbols = ['+', '-', '/', '*']
+    if (question === '') {
+      setQuestion("0.")
+      return
+    }
+    else if (question[question.length - 1] === '.') {
+      return;
+    }
+    else if (symbols.includes(question[question.length - 1])) {
+      setQuestion(question + '0.')
+    }
+    else {
+      let l = question.length - 1;
+      while (l >= 0) {
+        if (question[l] === '.') {
+          return
+        } else if (!symbols.includes(question[l])) {
+          l--;
+        }
+        else {
+          setQuestion(question + '.')
+          return
+        }
+      }
+      setQuestion(question + '.')
+    }
   }
 
   return (
     <>
       <div className='calculator'>
-        <div className='display-screen' id='display'>
+        <div className='display-screen'>
           <div className="que">
             <p>{question}</p>
           </div>
           <div className="ans">
-            <p>{answer}</p>
+            <p id='display'>{answer}</p>
           </div>
         </div>
         <div className='keys-container'>
@@ -52,7 +129,7 @@ function App() {
           <div className="key k16" id='equals' onClick={calculateAnswer}>=</div>
           <div className="key k17" id='zero' onClick={() => handleClick("0")}>0</div>
           <div className="key k18"></div>
-          <div className="key" id='decimal'>.</div>
+          <div className="key" id='decimal' onClick={handleDot}>.</div>
           <div className="key k20"></div>
         </div>
       </div>
